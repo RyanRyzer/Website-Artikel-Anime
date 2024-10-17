2d702936920496e1a5e195d57bf21eb2c2bc52a0
Ryan Robby <147597256+RyanRyzer@users.noreply.github.com>
2024-10-17 18:53:06 +0700

    Update script.js

--- Website Artikel Anime/script.js
+++ Website Artikel Anime/script.js
@@ -1,8 +1,13 @@
-window.addEventListener("scroll", function() {
-    const header = document.querySelector("header");
-    if (window.scrollY > 50) {
-      header.style.backgroundColor = "rgba(48, 52, 39, 0.7)"; // Transparan saat user melakukan scroll
-    } else {
-      header.style.backgroundColor = "#303427"; // Warna asli saat di atas
-    }
-  });
+const elements = document.querySelectorAll('.artikel, .card, .anime-item');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+}, {
+    threshold: 0.1
+});
+
+elements.forEach(el => observer.observe(el));

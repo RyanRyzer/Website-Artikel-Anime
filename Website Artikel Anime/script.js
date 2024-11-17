cccbba81fb1c46dce563e1f8c96756311f533fed
Ryan Robby <147597256+RyanRyzer@users.noreply.github.com>
2024-11-17 19:55:04 +0700

    Update script.js

--- Website Artikel Anime/script.js
+++ Website Artikel Anime/script.js
@@ -11,3 +11,12 @@ const observer = new IntersectionObserver(entries => {
 });
 
 elements.forEach(el => observer.observe(el));
+
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('header.navbar-container');
+    if (window.scrollY > 50) { // Ganti 50 dengan nilai scroll yang diinginkan
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
